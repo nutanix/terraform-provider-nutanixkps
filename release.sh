d2ac7e3e1fd137bea5aa2f2af74c1aea2d0c6ce5
skekre98 <sharvil.kekre@nutanix.com>
2021-04-01 13:56:12 -0700

    created release steps and added doc creation

--- release.sh
+++ release.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+# Prior to running the release script install tfplugindocs
+#  - https://github.com/hashicorp/terraform-plugin-docs#installation
+# Next run the following command to release a new version 
+#  - ./release.sh v<release number> 
+
 version=${1}
 echo ${version}
 if [ -z "${version}" ]
 then
     echo "version was left empty"
 else
-    # make installkpsclient
+    tfplugindocs generate
+    cp -r guides docs/
     git add .
     git commit -m "kps client install"
     git tag -a ${version} -m "Release version ${version}" main
